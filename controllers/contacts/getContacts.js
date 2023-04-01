58e6a59c91844d40a6afa4e473efae1a6abff552
Yevhen2701 <106544836+EvgenAltunin@users.noreply.github.com>
2023-04-01 23:51:54 +0300

    added contacts filtration by favorite

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -2,12 +2,23 @@ const { Contact } = require("../../models");
 
 const getContacts = async (req, res, next) => {
   const { _id } = req.user;
-  const { page = 1, limit = 10} = req.query;
+  const { page = 1, limit = 10, favorite} = req.query;
   const skip = (page - 1) * limit
-  const contacts = await Contact.find({ owner: _id }, "", {skip, limit: Number(limit)}).populate(
-    "owner",
-    "_id email subscription"
-  );
+
+if (favorite !== undefined && favorite !== "true" && favorite !== "false") {
+  return res.status(400).json({ message: "Invalid value for favorite field!" });
+}
+
+const filter = { owner: _id };
+if (favorite !== undefined) {
+  filter.favorite = favorite === "true";
+}
+  
+  console.log(filter)
+  const contacts = await Contact.find(filter, "", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "_id email subscription");
   res.status(200).json({ data: contacts });
 };
 
